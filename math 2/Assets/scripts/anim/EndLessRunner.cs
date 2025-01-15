93ad49ae8b3eeca75f9bca8027199477648d19cf
OpiumBird <145978078+MarcosMartinsCarvalho@users.noreply.github.com>
2025-01-15 14:36:54 +0100

    fix
    no problem

--- math 2/Assets/scripts/anim/EndLessRunner.cs
+++ math 2/Assets/scripts/anim/EndLessRunner.cs
@@ -8,9 +8,8 @@ public class EndlessRunner : MonoBehaviour
     Animator animator;
     enum State { grounded, airborne }
     State myState = State.grounded;
-    float time = 0;
     Vector3 velocity = Vector3.zero;
-    Vector3 gravity = Vector3.zero;
+    Vector3 gravity = new Vector3(0, -9.8f, 0);
     float y0;
     void Start()
     {
@@ -25,27 +24,39 @@ public class EndlessRunner : MonoBehaviour
             if (Input.GetMouseButtonUp(0))
             {
                 animator.Play("jump");
+<<<<<<< Updated upstream
                 velocity = new Vector3(0, 5, 0); // Initial jump velocity
                 gravity = new Vector3(0, -4, 0);  // Downward acceleration
+=======
+                velocity = new Vector3(0, 8f, 0);
+>>>>>>> Stashed changes
                 myState = State.airborne;
             }
         }
 
         if (myState == State.airborne)
         {
+<<<<<<< Updated upstream
             velocity += gravity * Time.deltaTime; // Apply gravity to velocity
             Jumper.transform.position += velocity * Time.deltaTime; // Add velocity to position
 
+=======
+            velocity += gravity * Time.deltaTime;
+            Jumper.transform.position += velocity * Time.deltaTime;
+>>>>>>> Stashed changes
             if (Jumper.transform.position.y <= y0)
             {
                 // Reset position and state when grounded
                 velocity = Vector3.zero;
-                gravity = Vector3.zero;
                 animator.Play("run");
                 Jumper.transform.position = new Vector3(Jumper.transform.position.x, y0, 0);
                 myState = State.grounded;
             }
         }
     }
+<<<<<<< Updated upstream
 
 }
+=======
+}
+>>>>>>> Stashed changes
